9dd1204394856e41d5901a31ebb74c4c36a3aa0b
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-25 14:44:52 +0400

    break

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,6 +38,7 @@ export class AppComponent {
         for (let i = 0; i < this.numberOfTeams; i++) {
           const randomIndex = Math.floor(Math.random() * allMembers.length);
           const member = allMembers.splice(randomIndex, 1)[0];
+          if (!member) break;
           if (this.teams[i]) {
             this.teams[i].push(member);
           } else {
